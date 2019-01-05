4ce73828fdbb13ae0b20c9f0ef2a4847c1fad3d1
tstansberry <tstansberry00@stuy.edu>
2019-01-05 09:40:52 -0500

    Got Jsoup working

---
+++ Scraper.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Scraper {
+  public static void main(String args[]) {
+    System.out.println("IT WORKS!");
+    System.out.println("Does it?");
+  }
+}
