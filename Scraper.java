dc2b6c8ee625906e35991dbbd9d0e0761cb65b88
tstansberry <tstansberry00@stuy.edu>
2019-01-06 19:35:52 -0500

    Connected scraper to dictionary.com, now outputs html from website

--- Scraper.java
+++ Scraper.java
@@ -1,14 +1,18 @@
 import java.io.IOException;
 
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Scraper {
-  public static void main(String args[]) {
-    System.out.println("IT WORKS!");
-    System.out.println("Does it?");
+  public static void main(String args[]) throws IOException {
+    System.out.println(getDefinition("help"));
+  }
+
+  public static String getDefinition(String word) throws IOException {
+    Document doc;
+    doc = Jsoup.connect("https://www.dictionary.com/browse/" + word.toLowerCase()).get();
+    return doc.toString();
   }
 }
