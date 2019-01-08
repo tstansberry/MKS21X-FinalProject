0f9fceb2eb1416b7b4a34fef6afc988d54a5fcf9
tstansberry <tstansberry00@stuy.edu>
2019-01-07 20:52:04 -0500

    Identified and formatted specific data that we need

--- Scraper.java
+++ Scraper.java
@@ -7,12 +7,16 @@ import org.jsoup.select.Elements;
 
 public class Scraper {
   public static void main(String args[]) throws IOException {
-    System.out.println(getDefinition("help"));
+    System.out.println(getDefinition(args[1]));
   }
 
   public static String getDefinition(String word) throws IOException {
     Document doc;
     doc = Jsoup.connect("https://www.dictionary.com/browse/" + word.toLowerCase()).get();
-    return doc.toString();
+    Elements metas = doc.getElementsByTag("meta");
+    Element data = metas.get(1);
+    String output = data.toString();
+    output = output.substring(34, output.length() - 11);
+    return output;
   }
 }
