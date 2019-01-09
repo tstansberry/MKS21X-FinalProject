d0bcf9ae37133923ce06f87da1391166772b62d7
tstansberry <tstansberry00@stuy.edu>
2019-01-09 16:53:51 -0500

    The user is now able to access ALL the possible definitions for the word given

--- Scraper.java
+++ Scraper.java
@@ -7,7 +7,7 @@ import org.jsoup.select.Elements;
 
 public class Scraper {
   public static void main(String args[]) throws IOException {
-    System.out.println(getDefinition("help", 0));
+    System.out.println(getDefinition(args[0], Integer.parseInt(args[1])));
   }
 
   public static String getDefinition(String word, int definitionNumber) throws IOException {
@@ -23,12 +23,26 @@ public class Scraper {
       Elements ol = doc.getElementsByTag("ol");
       Element targetInfo = ol.get(0);
       Elements li = targetInfo.getElementsByTag("li");
-      Element clutteredDefinition = li.get(definitionNumber);
-      String output = clutteredDefinition.getAttribute("value");
-      return output;
+      boolean found = false;
+      Element clutteredDefinition = null;
+      for (int x = 0; x < li.size() && ! found; x ++) {
+        clutteredDefinition = li.get(x);
+        if (checkValueTag(clutteredDefinition, definitionNumber)) found = true;
+      }
+      if (! found) throw new IndexOutOfBoundsException();
+      return clutteredDefinition.toString();
     }
     catch(IndexOutOfBoundsException e) {
       return "Uh oh, something went wrong. Please make sure your number and word are correct!";
     }
+  }
+
+  private static boolean checkValueTag(Element html, int target) {
+    target ++;
+    String htmlString = html.toString();
+    if (! htmlString.substring(4, 9).equals("value")) return false;
+    char targetChar = (char)(target + '0');
+    if (targetChar != htmlString.charAt(11)) return false;
+    return true;
   }
 }
