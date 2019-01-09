c97465cb86b1f099a1a063d7b72ad876f2776da1
tstansberry <tstansberry00@stuy.edu>
2019-01-09 07:16:35 -0500

    Changed up the search process, the parser now finds a less-accessible area that houses all of the definitions

--- Scraper.java
+++ Scraper.java
@@ -7,16 +7,28 @@ import org.jsoup.select.Elements;
 
 public class Scraper {
   public static void main(String args[]) throws IOException {
-    System.out.println(getDefinition(args[0]));
+    System.out.println(getDefinition("help", 0));
   }
 
-  public static String getDefinition(String word) throws IOException {
+  public static String getDefinition(String word, int definitionNumber) throws IOException {
     Document doc;
     doc = Jsoup.connect("https://www.dictionary.com/browse/" + word.toLowerCase()).get();
+    /*
     Elements metas = doc.getElementsByTag("meta");
     Element data = metas.get(1);
     String output = data.toString();
     output = output.substring(34, output.length() - 11);
-    return output;
+    */
+    try {
+      Elements ol = doc.getElementsByTag("ol");
+      Element targetInfo = ol.get(0);
+      Elements li = targetInfo.getElementsByTag("li");
+      Element clutteredDefinition = li.get(definitionNumber);
+      String output = clutteredDefinition.getAttribute("value");
+      return output;
+    }
+    catch(IndexOutOfBoundsException e) {
+      return "Uh oh, something went wrong. Please make sure your number and word are correct!";
+    }
   }
 }
