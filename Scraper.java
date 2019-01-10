da5c60510bd2771fb256beea90e30e680cb3dfc0
tstansberry <tstansberry00@stuy.edu>
2019-01-10 11:23:12 -0500

    Added more error testing

--- Scraper.java
+++ Scraper.java
@@ -11,15 +11,9 @@ public class Scraper {
   }
 
   public static String getDefinition(String word, int definitionNumber) throws IOException {
-    Document doc;
-    doc = Jsoup.connect("https://www.dictionary.com/browse/" + word.toLowerCase()).get();
-    /*
-    Elements metas = doc.getElementsByTag("meta");
-    Element data = metas.get(1);
-    String output = data.toString();
-    output = output.substring(34, output.length() - 11);
-    */
     try {
+      Document doc;
+      doc = Jsoup.connect("https://www.dictionary.com/browse/" + word.toLowerCase()).get();
       Elements ol = doc.getElementsByTag("ol");
       Element targetInfo = ol.get(0);
       Elements li = targetInfo.getElementsByTag("li");
@@ -33,6 +27,9 @@ public class Scraper {
       return clutteredDefinition.toString();
     }
     catch(IndexOutOfBoundsException e) {
+      return "Uh oh, something went wrong. Please make sure your number and word are correct!";
+    }
+    catch(org.jsoup.HttpStatusException e) {
       return "Uh oh, something went wrong. Please make sure your number and word are correct!";
     }
   }
