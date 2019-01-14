cdc3cebacc1cf65c8fb6516e7f4490130c5b2e41
vsofat <vsofat00@stuy.edu>
2019-01-14 00:38:00 -0500

    checkingout of branch

--- WorkingTerminal.java
+++ WorkingTerminal.java
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-//API : http://mabe02.github.io/lanterna/apidocs/2.1/
-import com.googlecode.lanterna.terminal.Terminal.SGR;
-import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.input.Key.Kind;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.Terminal.Color;
-import com.googlecode.lanterna.terminal.TerminalSize;
-import com.googlecode.lanterna.LanternaException;
-import com.googlecode.lanterna.input.CharacterPattern;
-import com.googlecode.lanterna.input.InputDecoder;
-import com.googlecode.lanterna.input.InputProvider;
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.input.KeyMappingProfile;
-=======
 //API : http://mabe02.github.io/lanterna/apidocs/3.0/
 import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.input.*;
@@ -24,112 +8,15 @@ import java.io.IOException;
 import java.awt.Color;
 import java.util.*;
 import java.io.*;
->>>>>>> Terminal
+
 
 public class TerminalDemo {
-
-<<<<<<< HEAD
-	public static void putString(int r, int c,Terminal t, String s){
-		t.moveCursor(r,c);
-		for(int i = 0; i < s.length();i++){
-            t.moveCursor(r,c);
-			t.putCharacter(s.charAt(i));
-		}
-	}
-	public static void main(String[] args) {
-
-
-		int x = 10;
-		int y = 10;
-
-		Terminal terminal = TerminalFacade.createTextTerminal();
-		terminal.enterPrivateMode();
-
-		TerminalSize size = terminal.getTerminalSize();
-		terminal.setCursorVisible(true);
-
-		boolean running = true;
-
-		long tStart = System.currentTimeMillis();
-		long lastSecond = 0;
-
-		while(running){
-
-			//terminal.moveCursor(x,y);
-			terminal.applyBackgroundColor(Terminal.Color.WHITE);
-			terminal.applyForegroundColor(Terminal.Color.BLACK);
-			//applySGR(a,b) for multiple modifiers (bold,blink) etc.
-			//terminal.applySGR(Terminal.SGR.ENTER_UNDERLINE);
-			//terminal.putCharacter('\u00a4');
-			//terminal.putCharacter(' ');
-			terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
-			terminal.applyForegroundColor(Terminal.Color.DEFAULT);
-			//terminal.applySGR(Terminal.SGR.RESET_ALL);
-
-
-		/*	terminal.moveCursor(size.getColumns()-5,5);
-			terminal.applyBackgroundColor(Terminal.Color.RED);
-			terminal.applyForegroundColor(Terminal.Color.YELLOW);
-			terminal.applySGR(Terminal.SGR.ENTER_BOLD);
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.putCharacter('\u262d');
-			terminal.putCharacter(' ');
-			terminal.moveCursor(size.getColumns()-5,6);
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
-			terminal.applyForegroundColor(Terminal.Color.DEFAULT);  */
-
-			Key key = terminal.readInput();
-
-			if (key != null)
-			{
-
-                boolean checker = false;
-                String output = "";
-
-				if (key.getKind() == Key.Kind.Escape) {
-
-					terminal.exitPrivateMode();
-					running = false;
-				}
-
-                if (key.getKind() == Key.Kind.NormalKey) {
-                    output += key.getCharacter();
-                    }
-
-                    //if (key.getKind() == Key.Kind.Enter) {
-                        putString(1,20,terminal,output);
-                    //}
-
-
-
-
-
-
-
-			//	putString(1,4,terminal,"["+key.getCharacter() +"]");
-			//	putString(1,1,terminal,key.getCharacter()+"");//to clear leftover letters pad withspaces
-			}
-/*
-			//DO EVEN WHEN NO KEY PRESSED:
-			long tEnd = System.currentTimeMillis();
-			long millis = tEnd - tStart;
-			putString(1,2,terminal,"Milliseconds since start of program: "+millis);
-			if(millis/1000 > lastSecond){
-				lastSecond = millis / 1000;
-				//one second has passed.
-				putString(1,3,terminal,"Seconds since start of program: "+lastSecond);
-=======
   public static void putString(int x, int y, Screen screen, String str) {
 		for (int i = 0; i < str.length(); ++i) {
 			screen.setCharacter(x+i, y, new TextCharacter(str.charAt(i)));
 		}
 	}
->>>>>>> Terminal
+
 
 	public static void main(String[] args) throws IOException {
 
@@ -151,7 +38,6 @@ public class TerminalDemo {
 
   while (checker) {
 
-    // Score Keeping
     long tEnd = System.currentTimeMillis();
     long millis = tEnd - tStart;
     if(millis/1000 > lastSecond){
@@ -160,8 +46,6 @@ public class TerminalDemo {
 
     }
 
-
-    // Resize World and Game
     TerminalSize testsize = screen.getTerminalSize();
     if (testsize.getRows() != size.getRows() || testsize.getColumns() != size.getColumns()) {
       screen.clear();
