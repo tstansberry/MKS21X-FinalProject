2ec0e7c948a4ae59d32cb6666d4797ca7f13c796
tstansberry <tstansberry00@stuy.edu>
2019-01-21 16:16:02 -0500

    Fully fixed  defintion scraper

--- Scraper.java
+++ Scraper.java
@@ -8,14 +8,14 @@ import org.jsoup.select.Elements;
 
 public class Scraper {
   public static void main(String args[]) throws IOException {
-    System.out.println(getDefinition("help", 0));
+    System.out.println(getDefinition("help"));
     //System.out.println(getRandomWord());
   }
 
   //Compiles all the methods into one, taking the place of main
   public static String master(String returnType, String word) throws IOException{
     String error = "Uh oh, something went wrong. Please make your inputs are formatted correctly: \"<return type> + <word>\"";
-    if (returnType.equals("definition")) return getAllDefinitions(word);//return getDefinition(word, 0);
+    if (returnType.equals("definition")) return getDefinition(word);//return getDefinition(word, 0);
     else if (returnType.equals("synonyms")) return getSynonyms(word);
     else {
       return error;
@@ -23,13 +23,20 @@ public class Scraper {
   }
 
   //Original code, gets a specific definition for a word
-  public static String getDefinition(String word, int definitionNumber) throws IOException {
+  public static String getDefinition(String word) throws IOException {
     String error = "Uh oh, something went wrong. Please make your inputs are formatted correctly: \"<return type> + <word>\"";
     try {
       Document doc;
       doc = Jsoup.connect("https://www.dictionary.com/browse/" + word.toLowerCase()).get();
       Elements els = doc.getElementsByClass("e1q3nk1v4");
-      return html2text(els.get(definitionNumber).toString());
+      //return html2text(els.get(definitionNumber).toString());
+      String output = "";
+      for (int x = 0; x < els.size(); x ++) {
+        String current = html2text(els.get(x).toString());
+        output += (x + 1) + ". " + current + "\n";
+      }
+      return output;
+      //Old code, before the html on dictionary.com was changed
       /*
       Elements ol = doc.getElementsByTag("ol");
       Element targetInfo = ol.get(0);
@@ -54,6 +61,24 @@ public class Scraper {
     }
   }
 
+  //Using the previous method, gets all the definitions for a word
+  /*
+  public static String getAllDefinitions(String word) throws IOException{
+    String error = "Uh oh, something went wrong. Please make your inputs are formatted correctly: \"<return type> + <word>\"";
+    String output = "";
+    boolean stopped = false;
+    for (int x = 0; ! stopped; x ++) {
+      if (! getDefinition(word, x).equals(error)) {
+        output += (x + 1) + ". " + getDefinition(word, x) + "\n";
+      }
+      else {
+        stopped = true;
+        if (x == 0) return error;
+      }
+    }
+    return output;
+  }*/
+
   //Uses thesaurus.com to get the synonyms for a given word
   public static String getSynonyms(String word) throws IOException{
     try {
@@ -74,23 +99,6 @@ public class Scraper {
     catch(org.jsoup.HttpStatusException e) {
       return "Uh oh, something went wrong. Please make your inputs are formatted correctly: \"<return type> + <word>\"";
     }
-  }
-
-  //Using the previous method, gets all the definitions for a word
-  public static String getAllDefinitions(String word) throws IOException{
-    String error = "Uh oh, something went wrong. Please make your inputs are formatted correctly: \"<return type> + <word>\"";
-    String output = "";
-    boolean stopped = false;
-    for (int x = 0; ! stopped; x ++) {
-      if (! getDefinition(word, x).equals(error)) {
-        output += (x + 1) + ". " + getDefinition(word, x) + "\n";
-      }
-      else {
-        stopped = true;
-        if (x == 0) return error;
-      }
-    }
-    return output;
   }
 
   //Helper function for getDefinition
