c8c8930dba665be002600129c0a069aac9379a91
vsofat <vsofat00@stuy.edu>
2019-01-21 10:50:39 -0500

    indented to organize code; added displays for all screens

--- TerminalDemo.java
+++ TerminalDemo.java
@@ -57,6 +57,7 @@ public static void main(String[] args) throws IOException {
   boolean SecondEnter = false;
   boolean modeGoing = true;
   String Strmode = "";
+  String control = "";
   String Inputclearer = ""; //
 
   TerminalSize size = screen.getTerminalSize();
@@ -102,207 +103,255 @@ public static void main(String[] args) throws IOException {
     if (key != null){
 
       // for all displays
-      /*    if (key.getKeyType() == KeyType.ArrowRight) {
-      display++;
-      display%=3;
-      screen.clear();
-      tEnd = System.currentTimeMillis();
-      millis = tEnd - tStart;
-      if(millis/1000 > lastSecond){
-      lastSecond = millis / 1000;
-      putString(1,3,screen,"Seconds since start of program: "+lastSecond);
+      if (key.getKeyType() == KeyType.ArrowRight) {
+        display++;
+        display%=6;
+        screen.clear();
+        tEnd = System.currentTimeMillis();
+        millis = tEnd - tStart;
+        if(millis/1000 > lastSecond){
+          lastSecond = millis / 1000;
+          putString(1,3,screen,"Seconds since start of program: "+lastSecond);
 
-    }
-  }
+        }
+      }
 
-  if (key.getKeyType() == KeyType.ArrowLeft) {
-  display--;
-  display%=3;//3 displays
-  screen.clear();
-  tEnd = System.currentTimeMillis();
-  millis = tEnd - tStart;
-  if(millis/1000 > lastSecond){
-  lastSecond = millis / 1000;
-  putString(1,3,screen,"Seconds since start of program: "+lastSecond);
+      if (key.getKeyType() == KeyType.ArrowLeft) {
+        display--;
+        display%=6;//3 displays
+        screen.clear();
+        tEnd = System.currentTimeMillis();
+        millis = tEnd - tStart;
+        if(millis/1000 > lastSecond){
+          lastSecond = millis / 1000;
+          putString(1,3,screen,"Seconds since start of program: "+lastSecond);
 
-}
-} */
+        }
+      }
 
-if (key.getKeyType() == KeyType.Escape){
-  checker = false;
-}
+      if (key.getKeyType() == KeyType.Escape){
+        checker = false;
+      }
 
-// for display 0
-if (display == 0){
+      // for display 0
+      if (display == 0){
 
-  String success  =  "The mode you have requested is: " + Strmode + "."; // successful input
-  String failure = "Sorry but that is not an available mode. Please try again."; // unsuccessful input
+        String success  =  "The mode you have requested is: " + Strmode + "."; // successful input
+        String failure = "Sorry but that is not an available mode. Please try again."; // unsuccessful input
 
-  if ((key.getKeyType() == KeyType.Character)) {
-    if (!firstEnterOver){
-      Strmode += key.getCharacter();
-      String reset = "                                                               ";
-      putString(1,13,screen,reset);
-    }
-  }
+        if ((key.getKeyType() == KeyType.Character)) {
+          if (!firstEnterOver){
+            Strmode += key.getCharacter();
+            String reset = "                                                               ";
+            putString(1,13,screen,reset);
+          }
+        }
 
-  if (key.getKeyType() == KeyType.Backspace) {
-    for (int i = 0; i < Strmode.length(); i++){
-      Inputclearer +=  " ";
-    }
-    putString(1,12,screen,Inputclearer);
-    Strmode = Strmode.substring(0, Strmode.length() - 1);
-    screen.refresh();
-  }
+        if (key.getKeyType() == KeyType.Backspace) {
+          for (int i = 0; i < Strmode.length(); i++){
+            Inputclearer +=  " ";
+          }
+          putString(1,12,screen,Inputclearer);
+          Strmode = Strmode.substring(0, Strmode.length() - 1);
+          screen.refresh();
+        }
 
-  screen.refresh();
+        screen.refresh();
 
-  if ((key.getKeyType() == KeyType.Enter)){
+        if ((key.getKeyType() == KeyType.Enter)){
 
-    for (int i = 0; i < Strmode.length(); i++){
-      Inputclearer +=  " ";
-    }
-    putString(1,12,screen, Inputclearer); // clears input (doesn't matter if it was valid or not)
-    screen.refresh();
+          for (int i = 0; i < Strmode.length(); i++){
+            Inputclearer +=  " ";
+          }
+          putString(1,12,screen, Inputclearer); // clears input (doesn't matter if it was valid or not)
+          screen.refresh();
 
-    if (Strmode.equals("[1]") || Strmode.equals("[2]") || Strmode.equals("[3]") || Strmode.equals("[4]")){ //checks if input is valid
-      putString(1,13,screen,success);
-      putString(1,14,screen,"Please check mode and press SPACE to proceed. You may press TAB to change your mode entry.");
-      firstEnterOver = true;
-    }
+          if (Strmode.equals("[1]") || Strmode.equals("[2]") || Strmode.equals("[3]") || Strmode.equals("[4]")){ //checks if input is valid
+            putString(1,13,screen,success);
+            putString(1,14,screen,"Please check mode and press SPACE to proceed. You may press TAB to change your mode entry.");
+            firstEnterOver = true;
+          }
 
-    else{
-      putString(1,13,screen,failure); // tells user input is invalid
-      Strmode = ""; // allows user to reenter a new input
-      screen.refresh();
-    }
+          else{
+            putString(1,13,screen,failure); // tells user input is invalid
+            Strmode = ""; // allows user to reenter a new input
+            screen.refresh();
+          }
 
-  }
+        }
 
-  if (key.getCharacter().equals(' ')){
-    if (firstEnterOver){
-      display = 1;
-      display%=4;
-      screen.clear();
-    }
-  }
-  if (key.getKeyType() ==  KeyType.Tab){
-    if (firstEnterOver){
-      for (int i = 0; i < success.length(); i++){
+        if (key.getCharacter().equals(' ')){
+          if (firstEnterOver){
+            display = 1;
+            display%=6;
+            screen.clear();
+          }
+        }
+        if (key.getKeyType() ==  KeyType.Tab){
+          if (firstEnterOver){
+            for (int i = 0; i < success.length(); i++){
+              Inputclearer +=  " ";
+            }
+            putString(1,13,screen, Inputclearer); // clears input (doesn't matter if it was valid or not)
+            Inputclearer = "";
+            for (int i = 0; i < "Please check mode and press SPACE to proceed. You may press TAB to change your mode entry.".length(); i++){
+              Inputclearer +=  " ";
+            }
+            putString(1,14,screen, Inputclearer);
+            screen.refresh();
+            firstEnterOver = false;
+            Strmode = "";
+
+          }
+        }
+
+
+      }
+
+      // for display 1
+      if (display == 1){
+
+        String success  =  "The word you have requested is: " + input + "."; // successful input
+        String failure = "Sorry but that word not correct. Please try again."; // unsuccessful input
+
+        if ((key.getKeyType() == KeyType.Character)) {
+          if (!SecondEnter){
+            input += key.getCharacter();
+          }
+        }
+
+        if (key.getKeyType() == KeyType.Backspace) {
+          for (int i = 0; i < input.length(); i++){
+            Inputclearer +=  " ";
+          }
+          putString(1,12,screen,Inputclearer);
+          input = input.substring(0, input.length() - 1);
+          screen.refresh();
+        }
+
+        screen.refresh();
+
+        if ((key.getKeyType() == KeyType.Enter)){
+
+          for (int i = 0; i < input.length(); i++){
+            Inputclearer +=  " ";
+          }
+          putString(1,8,screen, Inputclearer); // clears input (doesn't matter if it was valid or not)
+          screen.refresh();
+
+          SecondEnter = true;
+
+          putString(1,13,screen,success);
+          putString(1,14,screen,"Please check mode and press SPACE to proceed. You may press TAB to change your mode entry.");
+        }
+
+        if (key.getCharacter().equals(' ')){
+
+          if (SecondEnter){
+            if (Strmode.equals("[1]")){
+              display = 3;
+              display%=6;
+              screen.clear();
+            }
+
+            if (Strmode.equals("[2]")){
+              display = 4;
+              display%=6;
+              screen.clear();
+            }
+
+            if (Strmode.equals("[3]")){
+              display = 5;
+              display%=6;
+              screen.clear();
+            }
+
+            if (Strmode.equals("[4]")){
+              display = 6;
+              display%=6;
+              screen.clear();
+            }
+
+          }
+        }
+
+        if (key.getKeyType() ==  KeyType.Tab){
+
+          if (SecondEnter){
+            for (int i = 0; i < success.length(); i++){
+              Inputclearer +=  " ";
+            }
+            putString(1,13,screen, Inputclearer); // clears input (doesn't matter if it was valid or not)
+            Inputclearer = "";
+            for (int i = 0; i < "Please check mode and press SPACE to proceed. You may press TAB to change your mode entry.".length(); i++){
+              Inputclearer +=  " ";
+            }
+            putString(1,14,screen, Inputclearer);
+            screen.refresh();
+            SecondEnter = false;
+            input = "";
+
+          }
+
+        }
+
+      } // display 1 ends
+
+      // for display 2
+      if (display <= 3){
+
+        if ((key.getKeyType() == KeyType.Character)) {
+          //if (!firstEnterOver){
+          control += key.getCharacter();
+          /*  String reset = "                                                               ";
+          putString(1,13,screen,reset); */
+          //    }
+        }
+
+        if (key.getKeyType() == KeyType.Backspace) {
+          for (int i = 0; i < control.length(); i++){
+            Inputclearer +=  " ";
+          }
+          putString(1,12,screen,Inputclearer);
+          control = control.substring(0, control.length() - 1);
+          screen.refresh();
+        }
+
+        screen.refresh();
+
+        if ((key.getKeyType() == KeyType.Enter)){
+
+          for (int i = 0; i < Strmode.length(); i++){
+            Inputclearer +=  " ";
+          }
+          putString(1,12,screen, Inputclearer); // clears input (doesn't matter if it was valid or not)
+          screen.refresh();
+
+        }
+
+        if (key.getCharacter().equals(' ')){
+          display = 1;
+          display%=6;
+          screen.clear();
+        }
+      if (key.getKeyType() ==  KeyType.Tab){
+        /*if (firstEnterOver){
+        for (int i = 0; i < success.length(); i++){
         Inputclearer +=  " ";
       }
       putString(1,13,screen, Inputclearer); // clears input (doesn't matter if it was valid or not)
       Inputclearer = "";
       for (int i = 0; i < "Please check mode and press SPACE to proceed. You may press TAB to change your mode entry.".length(); i++){
-        Inputclearer +=  " ";
-      }
-      putString(1,14,screen, Inputclearer);
-      screen.refresh();
-      firstEnterOver = false;
-      Strmode = "";
-
+      Inputclearer +=  " ";
     }
-  }
+    putString(1,14,screen, Inputclearer);
+    screen.refresh();
+    firstEnterOver = false;
+    Strmode = "";
 
-
+  }*/
 }
-
-// for display 1
-if (display == 1){
-
-  String success  =  "The word you have requested is: " + input + "."; // successful input
-  String failure = "Sorry but that word not correct. Please try again."; // unsuccessful input
-
-
-  if ((key.getKeyType() == KeyType.Character)) {
-    if (!SecondEnter){
-      input += key.getCharacter();
-    }
-  }
-
-  if (key.getKeyType() == KeyType.Backspace) {
-    for (int i = 0; i < input.length(); i++){
-      Inputclearer +=  " ";
-    }
-    putString(1,12,screen,Inputclearer);
-    input = input.substring(0, input.length() - 1);
-    screen.refresh();
-  }
-
-  screen.refresh();
-
-  if ((key.getKeyType() == KeyType.Enter)){
-
-    for (int i = 0; i < input.length(); i++){
-      Inputclearer +=  " ";
-    }
-    putString(1,8,screen, Inputclearer); // clears input (doesn't matter if it was valid or not)
-    screen.refresh();
-
-    SecondEnter = true;
-
-    putString(1,13,screen,success);
-    putString(1,14,screen,"Please check mode and press SPACE to proceed. You may press TAB to change your mode entry.");
-  }
-
-
-  if (key.getCharacter().equals(' ')){
-
-    if (SecondEnter){
-      if (Strmode.equals("[1]")){
-        display += 1;
-        display%=5;
-        screen.clear();
-      }
-    }
-
-    if (SecondEnter){
-      if (Strmode.equals("[2]")){
-        display += 2;
-        display%=5;
-        screen.clear();
-      }
-    }
-
-    if (SecondEnter){
-      if (Strmode.equals("[3]")){
-        display += 3;
-        display%=5;
-        screen.clear();
-      }
-    }
-
-    if (SecondEnter){
-      if (Strmode.equals("[4]")){
-        display += 4;
-        display%=5;
-        screen.clear();
-      }
-    }
-
-  }
-
-  if (key.getKeyType() ==  KeyType.Tab){
-
-    if (SecondEnter){
-      for (int i = 0; i < success.length(); i++){
-        Inputclearer +=  " ";
-      }
-      putString(1,13,screen, Inputclearer); // clears input (doesn't matter if it was valid or not)
-      Inputclearer = "";
-      for (int i = 0; i < "Please check mode and press SPACE to proceed. You may press TAB to change your mode entry.".length(); i++){
-        Inputclearer +=  " ";
-      }
-      putString(1,14,screen, Inputclearer);
-      screen.refresh();
-      SecondEnter = false;
-      input = "";
-
-    }
-
-  }
-
-} // display 1 ends
+}
 
 } // input loop ends here
 
@@ -345,17 +394,21 @@ if (display == 1){
 }
 
 // code for display 2
-if (display == 2){
+if (display == 4){
+
   putString(1,5,screen,"The mode you have entered is:" + Strmode);
   putString(1,6,screen,"The word you have entered is:" + input);
-
+  putString(1,7,screen,"Type /'--controls'/ to see your options on how to proceed");
   //putString(1,17,screen, "Successful Mode Entry!");
+  putString(1,15,screen,control);
+  /*
   String result = Scraper.master("everything", input);
   String lookingFor = "\n";
   String replaceWith = "|";
   String newResult = result.replace(lookingFor,replaceWith);
   screen.doResizeIfNecessary();
-  putStringSpecial(1,10,screen,newResult);
+  putStringSpecial(1,18,screen,newResult);
+  */
 
 }
 
@@ -363,14 +416,17 @@ if (display == 2){
 if (display == 3){
   putString(1,5,screen,"The mode you have entered is:" + Strmode);
   putString(1,6,screen,"The word you have entered is:" + input);
-
+  putString(1,7,screen,"Type /'--controls'/ to see your options on how to proceed");
   //putString(1,17,screen, "Successful Mode Entry!");
-  String result = Scraper.master("definition", input);
+  putString(1,15,screen,control);
+  /*
+  String result = Scraper.master("everything", input);
   String lookingFor = "\n";
   String replaceWith = "|";
   String newResult = result.replace(lookingFor,replaceWith);
   screen.doResizeIfNecessary();
-  putStringSpecial(1,10,screen,newResult);
+  putStringSpecial(1,18,screen,newResult);
+  */
 
 }
 
@@ -389,20 +445,23 @@ if (display == 4){
 
 }
 
-// code for display 3
-/*if (display == 5){
-putString(1,5,screen,"The mode you have entered is:" + Strmode);
-putString(1,6,screen,"The word you have entered is:" + input);
+// code for display 5
+if (display == 5){
+  putString(1,5,screen,"The mode you have entered is:" + Strmode);
+  putString(1,6,screen,"The word you have entered is:" + input);
+  putString(1,7,screen,"Type /'--controls'/ to see your options on how to proceed");
+  //putString(1,17,screen, "Successful Mode Entry!");
+  putString(1,15,screen,control);
+  /*
+  String result = Scraper.master("everything", input);
+  String lookingFor = "\n";
+  String replaceWith = "|";
+  String newResult = result.replace(lookingFor,replaceWith);
+  screen.doResizeIfNecessary();
+  putStringSpecial(1,18,screen,newResult);
+  */
 
-//putString(1,17,screen, "Successful Mode Entry!");
-String result = Scraper.master("game", input);
-String lookingFor = "\n";
-String replaceWith = "|";
-String newResult = result.replace(lookingFor,replaceWith);
-screen.doResizeIfNecessary();
-putStringSpecial(1,10,screen,newResult);
-
-} */
+}
 
 screen.doResizeIfNecessary();
 
@@ -411,11 +470,9 @@ screen.refresh();
 }
 
 screen.stopScreen(); // loops comes here if ESC pressed
-}
 
 }
-
-
+}
 
 //_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 /*  if (firstEnterOver){
